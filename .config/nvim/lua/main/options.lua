c8bdb3718e9b527a6a7eb0ebb0633f48046a172a
x86mota <felipecruz.d@proton.me>
2025-02-18 01:07:35 -0300

    udpate nvim options

--- .config/nvim/lua/main/options.lua
+++ .config/nvim/lua/main/options.lua
@@ -2,7 +2,7 @@ vim.cmd('let g:netrw_liststyle = 3')
 
 local opt = vim.opt
 
--- Global
+-- {{{ Global
 opt.errorbells = false
 opt.title = false
 opt.encoding = 'utf-8'
@@ -10,39 +10,52 @@ opt.updatetime = 300
 opt.mouse = ""
 opt.laststatus = 2
 opt.showtabline = 2
+-- }}}
 
--- Editor
+-- {{{ Editor
 opt.number = true
 opt.cursorline = true
 opt.wrap = false
-opt.foldmethod = marker
+opt.foldmethod = 'marker'
+opt.foldmarker = {'{{{', '}}}'}
 opt.scrolloff = 10
 opt.backspace = 'indent,eol,start'
 opt.syntax = 'on'
 opt.filetype = 'indent', 'plugin', 'on'
+opt.list = true
+opt.listchars = { 
+    eol = '↲',
+    tab = '→ ',
+    trail = '•',
+    space = '·',
+}
+-- }}}
 
--- Indent
+-- {{{ Indent
 opt.autoindent = true
 opt.smartindent = true
+-- }}}
 
--- Tabulation
+-- {{{ Tabulation
 opt.expandtab = true
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
+-- }}}
 
--- Search
+-- {{{ Search
 opt.path = '.,**'
 opt.ignorecase = true
 opt.smartcase = true
 opt.incsearch = true
 opt.hlsearch = true
---vim.cmd('let @/=""')
+vim.fn.setreg('/', '')
 opt.wildmenu = true
 opt.wildmode = 'longest,full'
 opt.wildoptions = 'pum'
+-- }}}
 
--- Buffer
+-- {{{ Buffer
 opt.hidden = true
 opt.confirm = true
 opt.splitright = true
@@ -52,3 +65,4 @@ opt.backup = false
 opt.writebackup = false
 opt.undofile = true
 opt.undodir = '/tmp'
+-- }}}
