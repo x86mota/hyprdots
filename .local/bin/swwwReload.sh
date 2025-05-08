bda9dc457779a6ee058c182cea5c4098e7e9ab51
x86mota <felipecruz.d@proton.me>
2025-05-08 00:12:51 -0300

    fix: update swww script

---
+++ .local/bin/swwwReload.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function _endProcess {
+    local process="$1"
+
+    pgrep -f "${process}" | while read -r pid; do
+        kill "${pid}"
+    done
+}
+
+if pgrep -f "swwwLauncher.sh"; then
+    _endProcess "swwwLauncher.sh"
+    _endProcess "sleep"
+fi
+
+swwwLauncher.sh
